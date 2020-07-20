da9e87f1cf166a5b037ff9802d3d6a4994d25bc3
Luzaks <kanemekanik@gmail.com>
2020-07-20 15:43:46 -0500

    Feat: Create the Games List component and Implement the connected state.

--- src/components/GamesPage.js
+++ src/components/GamesPage.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import GamesList from './GamesList';
 
-const GamesPage = () => {
-    return (
-        <div>
-            hey i'm on games page
-        </div>
-    )
-};
+const mapStateToProps = (state) => { return { games: state.games }};
+
+class GamesPage extends React.Component {
+    render() {
+        return (
+            <div>
+
+                <GamesList  games={this.props.games}/>
+            </div>
+        )
+    }
+}
 
 GamesPage.propTypes = {
     games: PropTypes.array.isRequired,
 };
 
-const mapStateToProps = (state) => { return { games: state.games }};
-
-connect(mapStateToProps)(GamesPage);
-
-export default GamesPage;
+export default connect(mapStateToProps)(GamesPage);
