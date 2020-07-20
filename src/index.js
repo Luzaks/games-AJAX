7a89fa3f61a0aa9d56e93b5d230950070cf97eff
Luzaks <kanemekanik@gmail.com>
2020-07-20 13:22:05 -0500

    Feat: Set-up redux store, create rootReducer.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {applyMiddleware, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import {composeWithDevTools} from "redux-devtools-extension";
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
